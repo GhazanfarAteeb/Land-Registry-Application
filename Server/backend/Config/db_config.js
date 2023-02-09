4405e1d846bbb701e71cb0d0725cd447bad0f747
Ghazanfar Ateeb <ghazanfar.ateeb9125@gmail.com>
2023-02-09 15:15:00 +0500

    first commit

---
+++ Server/backend/Config/db_config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  email: "strange.person9125@gmail.com",
+  password: "@Killer9125",
+  NEXMO_API_KEY: "aba4c3cd",
+  NEXMO_API_SECRET: "2X0ZbLeXSuuNILkj",
+  NEXMO_FROM_NUMBER: "+923109453603",
+  MongoURI: `mongodb+srv://GhazanfarAteeb9125:LaBK2bJJhuYeLoCM@landregisterysystem.fkafhhl.mongodb.net/LRS?retryWrites=true&w=majority`
+}
